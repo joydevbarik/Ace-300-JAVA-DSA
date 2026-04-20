c886437f150f00b84576a8a0a617bf15755bed18
Joydev Barik <barikjoydev263@gmail.com>
2026-04-20 13:59:17 +0530

    isSubsequence

---
+++ Day-10/subSequence.java
@@ -0,0 +1,24 @@
+public class subSequence {
+
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+
+        boolean result = isSubsequence(s, t);
+
+        System.out.println("Is Subsequence: " + result);
+    }
+}
