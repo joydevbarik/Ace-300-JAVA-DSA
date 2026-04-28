9089882b30f8562baf3bc0cfd27085ec156d7c47
Joydev Barik <barikjoydev263@gmail.com>
2026-04-29 00:00:19 +0530

    hammingWeight

---
+++ Day-17/hamming.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class hamming {
+
+    public static int hammingWeight(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            count += (n & 1);  
+            n >>>= 1;          
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int n = sc.nextInt();
+
+        int result = hammingWeight(n);
+
+        System.out.println("Number of 1 bits: " + result);
+
+        sc.close();
+    }
+}
