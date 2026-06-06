73c96e0083a3c282fac5abee9a508b09de9c0d25
Joydev Barik <barikjoydev263@gmail.com>
2026-06-06 06:22:43 +0530

    Trap water

---
+++ Day-52/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static int trap(int[] height) {
+
+        int water = 0;
+
+        for (int i = 0; i < height.length; i++) {
+
+            int leftMax = 0;
+            int rightMax = 0;
+
+            for (int j = 0; j <= i; j++) {
+                leftMax = Math.max(leftMax, height[j]);
+            }
+
+            for (int j = i; j < height.length; j++) {
+                rightMax = Math.max(rightMax, height[j]);
+            }
+
+            water += Math.min(leftMax, rightMax) - height[i];
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+
+        System.out.println(trap(height));
+    }
+}
