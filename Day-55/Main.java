5f6b41cb4961ae2006c1f6c29e327183ed86a17e
Joydev Barik <barikjoydev263@gmail.com>
2026-06-10 12:43:55 +0530

    Longest Substring Without Repeating Characters by Sliding Window + Hash Set

---
+++ Day-55/Main.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+
+public class Main {
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+
+            if (right - left + 1 > max) {
+                max = right - left + 1;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "abcabcbb";
+
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
