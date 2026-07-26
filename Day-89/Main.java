f56594af716acac822d89b27c5ed736d5e30a703
Joydev Barik <barikjoydev263@gmail.com>
2026-07-26 19:56:38 +0530

    Evaluate reverse polish notation

---
+++ Day-89/Main.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class Main {
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String s : tokens) {
+            if (s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            } else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String[] tokens = {"2", "1", "+", "3", "*"};
+        System.out.println(evalRPN(tokens)); // Output: 9
+    }
+}
