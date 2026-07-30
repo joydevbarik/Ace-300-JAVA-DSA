ed5285b56dd93a38df15d05ff1cdcec67f392d51
Joydev Barik <barikjoydev263@gmail.com>
2026-07-30 20:42:29 +0530

    Daily temperatures by brute force approach

---
+++ Day-93/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    answer[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+
+        int[] result = dailyTemperatures(temperatures);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
